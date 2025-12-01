b7edcd7d8b2beb882ba4d38d3ec4c6f21c49608d
yogirajb <108505047+yogirajb@users.noreply.github.com>
2025-12-02 01:11:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1001935710027'))
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://yogirajmr33:yogiraj2614@cluster0.4vr7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://yogirajmr33:yogiraj2614@cluster0.4vr7f.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "mzbotz")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mzmoviiez')
 
