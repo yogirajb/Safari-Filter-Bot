858a95e39239a5435c339df464aee00b4d1845e8
yogirajb <108505047+yogirajb@users.noreply.github.com>
2026-04-17 13:35:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,12 +45,12 @@ SEND_ALL_LIMITE = int(environ.get('SEND_ALL_LIMITE', 3))
 LIMIT_MODE = is_enabled((environ.get('LIMIT_MODE', 'True')), False)
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6139607609 5673859971').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5673859971').split()]
 OWNER_USER_NAME = environ.get("OWNER_USER_NAME", "SamMarathi") # widout 👉 @
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001882603026').split()]
 # post channel auto post new movie
 POST_CHANNELS = list(map(int, (channel.strip() for channel in environ.get('POST_CHANNELS', '').split(','))))
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1001716881922'))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1001664191895'))
 AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1001935710027'))
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
