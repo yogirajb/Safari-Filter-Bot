f0e4875fcacccf3295c570692ffb4674a036b046
yogirajb <108505047+yogirajb@users.noreply.github.com>
2026-04-18 23:57:42 +0530

    Update autojoin.py

--- plugins/Dev_Feature/autojoin.py
+++ plugins/Dev_Feature/autojoin.py
@@ -3,18 +3,15 @@
 import pyrogram
 from pyrogram import Client, filters
 
-from info import AUTH_CHANNELS
-
 @Client.on_chat_join_request()
 async def auto_accept_request(client, chat_member_update):
     chat_id = chat_member_update.chat.id
     user_id = chat_member_update.from_user.id
 
-    # ❌ in sab channels me auto approve band
-    if chat_id in AUTH_CHANNELS:
-        return
-
     try:
+        await client.get_chat_member(chat_id, client.me.id) 
         await client.approve_chat_join_request(chat_id, user_id)
+        #await client.send_message(user_id, f"ʜᴇʏ {chat_member_update.from_user.mention}!\nʀᴇǫᴜᴇsᴛ ʜᴀs ʙᴇᴇɴ ᴀᴄᴄᴇᴘᴛᴇᴅ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ {chat_member_update.chat.title}")
+
     except Exception as e:
-        print(e)
+        print(f"Error approving request: {e}")
