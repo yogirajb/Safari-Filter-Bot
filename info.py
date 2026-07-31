5092d957eb4a587df6718c641d94789353570a67
yogirajb <108505047+yogirajb@users.noreply.github.com>
2026-07-31 09:27:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -70,20 +70,20 @@ STREAM_MODE = is_enabled((environ.get('STREAM_MODE', "True")), False)
 #verify site api and url
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'True')), False)
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-VERIFY_URL = environ.get('VERIFY_URL', 'papajiurl.com')
-VERIFY_API = (environ.get('VERIFY_API', '41ff6d51799b604c63f6cfe75eb5b7a58794a850'))
+VERIFY_URL = environ.get('VERIFY_URL', 'arolinks.com')
+VERIFY_API = (environ.get('VERIFY_API', 'f565edfbea9e9a7e0ca7a758bbb580bf614d4487'))
 
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "3600"))
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "900"))
 VERIFY_URL2 = environ.get('VERIFY_URL2', 'shortxlinks.com')
 VERIFY_API2 = (environ.get('VERIFY_API2', '3b623c80e2c2534a5eae0bae35777c4c1aedd154'))
  
-THIRD_VERIFY_GAP = int(environ.get('THIRD_VERIFY_GAP', "3600"))
-VERIFY_URL3 = environ.get('VERIFY_URL3', 'inshorturl.com')
-VERIFY_API3 = (environ.get('VERIFY_API3', 'f0b67e13bda5380f9b4d252b3c82092ec456966e'))
+THIRD_VERIFY_GAP = int(environ.get('THIRD_VERIFY_GAP', "900"))
+VERIFY_URL3 = environ.get('VERIFY_URL3', 'vplink.in')
+VERIFY_API3 = (environ.get('VERIFY_API3', '1aa2fcb77f78adfec5c65ab78678f4492ddc584d'))
  
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtoopennlinks/19')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtoopennlinks/25')
 TUTORIAL2 = environ.get('TUTORIAL2', 'https://t.me/howtoopennlinks/21')
-TUTORIAL3 = environ.get('TUTORIAL3', 'https://t.me/howtoopennlinks/20')
+TUTORIAL3 = environ.get('TUTORIAL3', 'https://t.me/howtoopennlinks/26')
 
 # auto files delete
 AUTO_FILE_DELETE = is_enabled((environ.get('AUTO_FILE_DELETE', "True")), False)
