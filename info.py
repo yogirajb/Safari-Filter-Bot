1399a9998582e565ebc7d4a8be25683f6e6f738d
yogirajb <108505047+yogirajb@users.noreply.github.com>
2026-08-19 21:50:16 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,149 @@
+# This code has been modified by @Safaridev
+# Please do not remove this credit
+import re
+import os
+from os import environ, getenv
+from Script import script 
+
+id_pattern = re.compile(r'^.\d+$')
+
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+
+# Bot information
+SESSION = environ.get('SESSION', 'Media_search')
+API_ID = int(environ.get('API_ID', "13441344"))
+API_HASH = environ.get('API_HASH', "2f10533d9068507d0c10bf1074527167")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
+TIMEZONE = environ.get("TIMEZONE", "Asia/Kolkata")
+
+# Bot settings
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+USE_CAPTION_FILTER = is_enabled((environ.get('USE_CAPTION_FILTER', 'True')), True)
+PICS = (environ.get('PICS', 'https://telegra.ph/file/0ed351c8605c23e8cae79.jpg https://telegra.ph/file/6524270c008b60f81f30a.jpg https://telegra.ph/file/848ed57090fd5111ce64d.jpg https://telegra.ph/file/5fe959d96fcc33d1b9dc9.jpg https://telegra.ph/file/ec5f5a031b7826e28360c.jpg https://telegra.ph/file/aa4b77441bb41cfce12d7.jpg https://telegra.ph/file/00ed60e2c89d564d850ef.jpg https://telegra.ph/file/a5d518f6020976bc45264.jpg https://telegra.ph/file/650191ad1f813ca8f41cb.jpg https://telegra.ph/file/c6042704a1bc0a2b52996.jpg https://telegra.ph/file/9d68211fa9dcb208200be.jpg')).split()
+WELCOME_VID = environ.get("WELCOME_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
+
+#premium imag
+REFFER_PIC = environ.get('REFFER_PIC', 'https://graph.org/file/f75feb19aece0d4badefd.jpg')
+PREMIUM_PIC = environ.get('SUBSCRIPTION', 'https://i.imghippo.com/files/wPdPK1726559453.jpg')
+QR_CODE = environ.get('QR_CODE', 'https://envs.sh/bNP.jpg') # Scanner Code image 
+#refer time, or feffer count
+REFERAL_TIME = int(environ.get('REFERAL_USER_TIME', "2592000")) # set in seconds | already seted 1 month premium
+REFFER_POINT = int(environ.get('USER_POINT', "50")) # Set Referel point Count 
+#premium Users Satuts
+premium = environ.get('PREMIUM_LOGS', '-1001935710027')
+PREMIUM_LOGS = int(premium) if premium and id_pattern.search(premium) else None
+# lock file, set file limit 
+FILE_LIMITE = int(environ.get('FILE_LIMITE', 15))
+SEND_ALL_LIMITE = int(environ.get('SEND_ALL_LIMITE', 3))
+LIMIT_MODE = is_enabled((environ.get('LIMIT_MODE', 'True')), False)
+
+# Admins, Channels & Users
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5673859971').split()]
+OWNER_USER_NAME = environ.get("OWNER_USER_NAME", "SamMarathi") # widout 👉 @
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001882603026').split()]
+# post channel auto post new movie
+POST_CHANNELS = list(map(int, (channel.strip() for channel in environ.get('POST_CHANNELS', '-1002328506195').split(','))))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1001664191895'))
+AUTH_CHANNELS = [int(ch) for ch in environ.get('AUTH_CHANNELS', '-1001664191895 -1003095656831 -1003018299916').split()]
+AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1001935710027'))
+NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
+
+# MongoDB information
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://yogirajmr33:l0D1RxuCl4LKq4Bb@cluster0.4vr7f.mongodb.net/?appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "mzbotz")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mzmoviiez')
+
+#stream link shortner
+STREAM_SITE = (environ.get('STREAM_SITE', ''))
+STREAM_API = (environ.get('STREAM_API', ''))
+STREAM_HTO = (environ.get('STREAMHTO', 'https://t.me/MzBotz'))
+STREAM_MODE = is_enabled((environ.get('STREAM_MODE', "True")), False)
+
+
+#verify site api and url
+IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'True')), False)
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
+VERIFY_URL = environ.get('VERIFY_URL', 'arolinks.com')
+VERIFY_API = (environ.get('VERIFY_API', 'f565edfbea9e9a7e0ca7a758bbb580bf614d4487'))
+
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "900"))
+VERIFY_URL2 = environ.get('VERIFY_URL2', 'shortxlinks.com')
+VERIFY_API2 = (environ.get('VERIFY_API2', '3b623c80e2c2534a5eae0bae35777c4c1aedd154'))
+ 
+THIRD_VERIFY_GAP = int(environ.get('THIRD_VERIFY_GAP', "900"))
+VERIFY_URL3 = environ.get('VERIFY_URL3', 'vplink.in')
+VERIFY_API3 = (environ.get('VERIFY_API3', '1aa2fcb77f78adfec5c65ab78678f4492ddc584d'))
+ 
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtoopennlinks/25')
+TUTORIAL2 = environ.get('TUTORIAL2', 'https://t.me/howtoopennlinks/21')
+TUTORIAL3 = environ.get('TUTORIAL3', 'https://t.me/howtoopennlinks/26')
+
+# auto files delete
+AUTO_FILE_DELETE = is_enabled((environ.get('AUTO_FILE_DELETE', "True")), False)
+
+DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '').split()]
+MAX_B_TN = environ.get("MAX_B_TN", "7")
+MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
+PORT = int(environ.get("PORT", 8080))
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/mzmoviiezrequest')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MzBotz')
+MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001962983271))
+GROUP_VERIFY_LOGS = int(environ.get('GROUP_VERIFY_LOGS', -1001935710027)) # Group verify stats 
+REQ_CHANNEL = int(environ.get('REQ_CHANNEL', -1002150303936)) # movies request channel, else log channel
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '-1002387745957')
+IMDB = is_enabled((environ.get('IMDB', "True")), True)
+# TMDB settings
+TMDB_API_KEY = environ.get('TMDB_API_KEY', '')
+TMDB_LANGUAGE = environ.get('TMDB_LANGUAGE', 'en')  # optional, default English
+TMDB_POSTER_BASE = environ.get('TMDB_POSTER_BASE', 'https://image.tmdb.org/t/p/w500')
+AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
+AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
+PM_FILTER = is_enabled((environ.get('PM_FILTER', "True")), False)
+
+SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
+LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
+SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
+MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
+
+REACTION = ["🔥", "❤️", "😍", "⚡", "👍", "❤", "🔥", "🥰", "👏", "😁", "🎉", "🤩", "🙏", "👌", "🕊", "❤‍🔥", "⚡", "😇", "🤗", "😘", "🙊", "😎"]
+
+# Streaming & Koyeb FQDN Configuration
+BIN_CHANNEL = int(environ.get("BIN_CHANNEL", "-1001935710027")) 
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+
+# Clean FQDN & Auto HTTPS
+RAW_FQDN = getenv('FQDN', '')
+if RAW_FQDN:
+    FQDN = RAW_FQDN.replace('https://', '').replace('http://', '').strip('/')
+    URL = f"https://{FQDN}"
+else:
+    FQDN = BIND_ADRESS
+    URL = f"http://{BIND_ADRESS}:{PORT}"
+
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+WORKERS = int(environ.get('WORKERS', '4'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'safaribotts'))
+MULTI_CLIENT = False
+name = str(environ.get('name', 'safaribotts'))
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
+
+LOG_STR = "Current Cusomized Configurations are:-\n"
+LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
+LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n")
+LOG_STR += (f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n")
+LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_IMDB_DESCRIPTION is disabled , Plot will be shorter.\n")
+LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
+LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
+LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
